ef3f5b2b13ef679d3a1d0f8b1003f52a621df5c0
Connor Saunders <connorsaunders18@gmail.com>
2023-04-29 15:38:59 -0400

    Changed name of function 2.0

--- Scraper.py
+++ Scraper.py
@@ -7,25 +7,24 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 def scrape_prices():
-    # Create a ChromeDriver instance using ChromeDriverManager
+    # Grabs webdriver
     driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 
     # Navigate to the website
     driver.get('https://www.collegesimply.com/')
 
     # Wait for the search bar to be located on the page
-    wait = WebDriverWait(driver, 5)
+    wait = WebDriverWait(driver, 2)
     search_bar = wait.until(EC.presence_of_element_located((By.ID, "searchbar-nav")))
 
     # Click on the search bar to activate it
     search_bar.click()
-
     search_bar.send_keys("West Chester University")
 
     time.sleep(1)
 
     results = driver.find_element_by_css_selector("#searchbar-nav-results-container a")
-    time.sleep(2)
+    time.sleep(1)
     results.click()
 
     current_url = driver.current_url
@@ -34,22 +33,22 @@ def scrape_prices():
     time.sleep(1)
 
     driver.get(new_url)
-
+    # Grabs in-state price
     instate = driver.find_element_by_xpath('//div[@class="h2 mb-4"]')
     instate = instate.text
-
+    # Grabs in-state tuition
     tuition = driver.find_element_by_xpath('//td[@class="text-right font-weight-bold"]')
     tuition = tuition.text
-
+    # Grabs in-state books and supplies
     element = driver.find_element_by_xpath("//tr[2]/td[2]")
     bookcost = element.text
-
+    # Grabs in-state other fees
     element = driver.find_element_by_xpath("//tr[3]/td[2]")
     other_fees = element.text
-
+    # Grabs in-state room and board
     element = driver.find_element_by_xpath("//tr[4]/td[2]")
     room_board = element.text
-
+    # Grabs in-state other expenses
     element = driver.find_element_by_xpath("//tr[5]/td[2]")
     other_expenses = element.text
 
@@ -61,4 +60,4 @@ def scrape_prices():
     
     return instate, tuition, bookcost, other_fees, room_board, other_expenses
 
-scrape_wcu_prices()
+scrape_prices()
