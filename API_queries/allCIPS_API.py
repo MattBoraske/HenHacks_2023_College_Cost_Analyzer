f67ddac60aa52867ccf8366b062ed4b79b3dd7a5
Connor Saunders <connorsaunders18@gmail.com>
2023-04-29 21:52:48 -0400

    CIP Codes

--- API_queries/allCIPS_API.py
+++ API_queries/allCIPS_API.py
@@ -13,8 +13,12 @@ params = {
 
 response = requests.get(base_url, params=params)
 
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-else:
-    print(f'Error: {response.status_code}')
+data = response.json()
+
+results = data['results']
+for result in results:
+    programs = result['latest.programs.cip_4_digit']
+    for program in programs:
+        code = program['code']
+        title = program['title']
+        print(code, title)
